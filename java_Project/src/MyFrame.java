366e7a2bc4eb6f46fd4a863cff1c24c5efe8bd58
arborea <asic@E19>
2016-06-02 20:59:31 +0900

    이벤트 텍스트

---
+++ java_Project/src/MyFrame.java
@@ -0,0 +1,51 @@
+
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+class Mypanel extends JPanel {
+	BufferedImage img = null;
+	int img_x = 0, img_y = 0;
+
+	public Mypanel() {
+		try {
+			img = ImageIO.read(new File("car.gif"));
+		} catch (IOException e) {
+			System.out.println("no image");
+			System.exit(1);
+		}
+		addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				img_x = e.getX();
+				img_y = e.getY();
+				repaint();
+			}
+		});
+
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(img, img_x, img_y, null);
+	}
+
+}
+
+public class MyFrame extends JFrame {
+	public MyFrame() {
+		add(new Mypanel());
+		setSize(300, 500);
+		setVisible(true);
+	}
+
+	public static void main(String[] arg) {
+		new MyFrame();
+	}
+}
